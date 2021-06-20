3929785fcf612367daa52150a7cae3433e07733b
Shreya Shrestha <shreya2057@gmail.com>
2021-06-20 23:05:07 +0545

    Delete Alert Dialog added

--- lib/app/modules/user_list/views/user_list_view.dart
+++ lib/app/modules/user_list/views/user_list_view.dart
@@ -1,3 +1,4 @@
+import 'package:eventrack_app/app/modules/user_list/views/components/dialog.dart';
 import 'package:eventrack_app/app/modules/user_list/views/user_list.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +8,6 @@ import '../controllers/user_list_controller.dart';
 
 class UserListView extends GetView<UserListController> {
   final UserListController userListController = Get.find<UserListController>();
-  List <String> values = ['Shreya', 'Rose', 'Alex'];
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +19,7 @@ class UserListView extends GetView<UserListController> {
       body: ListView.builder(
         itemCount: userListController.users.length,
         itemBuilder: (context, index){
-          final item = values[index];
+          final item = userListController.users[index].toString();
           return Dismissible(
             key: Key(item), 
             child: ListTile(
@@ -27,57 +27,26 @@ class UserListView extends GetView<UserListController> {
               title: UserListCard(username: '${userListController.users[index].username}'),
             ),
              background: Container(
-               child: Row(
-                 children: [
-                   Icon(Icons.delete),
-                   Text("Delete")
-                 ],
-               ),
-               color: Colors.red
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(Icons.delete),
+                  Text(
+                    "Delete",
+                  )
+                ],
               ),
-              secondaryBackground: Container(
-               child: Row(
-                 mainAxisAlignment: MainAxisAlignment.end,
-                 children: [
-                   Icon(Icons.delete),
-                   Text("Delete")
-                 ],
-               ),
-               
-               color: Colors.red
-              ),
-              confirmDismiss: (direction) async {
-          return showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            content: Text(
-                "Are you sure you want to delete ?"),
-            actions: <Widget>[
-              FlatButton(
-                child: Text(
-                  "Cancel",
-                  style: TextStyle(color: Colors.black),
-                ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-              FlatButton(
-                child: Text(
-                  "Delete",
-                  style: TextStyle(color: Colors.red),
-                ),
-                onPressed: () {
-                  // TODO: Delete the item from DB etc..
-                  Navigator.of(context).pop();
-                  print("item deleted");
-                },
-              ),
-            ],
-          );
-        });
-  } 
+            ),
+            direction: DismissDirection.endToStart,
+            confirmDismiss: (direction) async {
+              return showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return DeleteDialog();
+                  }
+            );
+          } 
           );
         },
         
@@ -86,3 +55,4 @@ class UserListView extends GetView<UserListController> {
 
   }
 }
+
