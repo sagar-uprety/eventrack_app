246645f34d4b8ada60b1eca55b9afe3cbd0e852b
Nista Simkhada <58097961+nistasimkhada977@users.noreply.github.com>
2021-07-24 20:40:35 +0545

    Register button in event detail completed

---
+++ lib/app/modules/event_detail/views/Button/RoundedButton.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../../../../utilities/theme.dart';
+import '../../../../utilities/colors.dart';
+
+class RoundButton extends StatelessWidget {
+  final String text;
+  final onPress;
+  final double radius;
+  const RoundButton({
+    required this.text,
+    this.onPress,
+    this.radius = 30,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 9),
+      width: size.width * 0.8,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(radius),
+        child: FlatButton(
+          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+          color: AppColors.purple,
+          onPressed: onPress,
+          child: Text(text,
+              style: TextStyle(
+                color: Colors.white,
+              )),
+        ),
+      ),
+    );
+  }
+}
