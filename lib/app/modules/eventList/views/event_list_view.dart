8e0549d3278b6895de0f797c366978d16a4ad446
Shreya Shrestha <shreya2057@gmail.com>
2021-07-25 00:05:49 +0545

    Unfocus TextField

--- lib/app/modules/eventList/views/event_list_view.dart
+++ lib/app/modules/eventList/views/event_list_view.dart
@@ -41,151 +41,159 @@ List<String> categoriesList = [
 class EventListView extends GetView<EventListController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: ETAppBar(
-        title: 'Explore Events',
-        addBackButton: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Form(
-              key: controller.searchFormKey,
-              child: Column(
-                children: [
-                  FormInputField(
-                    label: 'Search',
-                    controller: controller.searchText,
-                    suffixIcon: Icons.search,
-                    onClickedSuffix: controller.search,
-                  ),
-                  
-                  ExpandablePanel(
-                theme: const ExpandableThemeData(
-                  headerAlignment: ExpandablePanelHeaderAlignment.center,
-                  tapBodyToExpand: true,
-                  tapBodyToCollapse: true,
-                  hasIcon: false,
-                ),
-                header: Container(
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              "Filter",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1!
-                                  .copyWith(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold
-                                  ),
-                            ),
-                            Icon(
-                              Icons.filter_alt,
-                            ),
-                          ],
-                        ),
-                      ),
-                     
-                    ],
-                  ),
-                ),
-                collapsed: Container(
-                ),
-                expanded: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+    return GestureDetector(
+      onTap: (){
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        appBar: ETAppBar(
+          title: 'Explore Events',
+          addBackButton: true,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Form(
+                key: controller.searchFormKey,
+                child: Column(
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    FormInputField(
+                      label: 'Search',
+                      controller: controller.searchText,
+                      suffixIcon: Icons.search,
+                      onClickedSuffix: controller.search,
+                    ),
+                    
+                    ExpandablePanel(
+                  theme: const ExpandableThemeData(
+                    headerAlignment: ExpandablePanelHeaderAlignment.center,
+                    tapBodyToExpand: true,
+                    tapBodyToCollapse: true,
+                    hasIcon: false,
+                  ),
+                  header: Container(
+                    child: Row(
                       children: [
-                        Row(
-                          children: [
-                            Text(
-                              'Catergories',
-                              style: Theme.of(context)
-                                      .textTheme
-                                      .bodyText1!
-                                      .copyWith(
-                                        fontWeight: FontWeight.w600,
-                                        color: Colors.black,
-                                      ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 50,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            shrinkWrap: true,
-                            padding: EdgeInsets.symmetric(horizontal: 0),
-                            itemCount: categoriesList.length,
-                            itemBuilder: (_, i) => MaterialButton(
-                              color: colors[i],
-                              child: Text(
-                                categoriesList[i],
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                "Filter",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1!
+                                    .copyWith(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold
+                                    ),
                               ),
-                              onPressed: ()=>{
-                                print("HI"),
-                              },
-                            ).paddingOnly(right: 10, bottom: 5),
+                              Icon(
+                                Icons.filter_alt,
+                              ),
+                            ],
                           ),
-                        ).paddingSymmetric(vertical: 10),
+                        ),
+                       
                       ],
                     ),
-                    SizedBox(
-                      height: 10
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.filter_list),
-                        Text(
-                          'Sort by Date',
-                          style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1!
-                                  .copyWith(
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.black,
-                                  ),
-                        ),
-                        SizedBox(
-                          height: 25,
-                          child: MaterialButton(
-                            child: Text(
-                            'Select Date',
+                  ),
+                  collapsed: Container(
+                  ),
+                  expanded: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                'Catergories',
+                                style: Theme.of(context)
+                                        .textTheme
+                                        .bodyText1!
+                                        .copyWith(
+                                          fontWeight: FontWeight.w600,
+                                          color: Colors.black,
+                                        ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 50,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              shrinkWrap: true,
+                              padding: EdgeInsets.symmetric(horizontal: 0),
+                              itemCount: categoriesList.length,
+                              itemBuilder: (_, i) => MaterialButton(
+                                color: colors[i],
+                                child: Text(
+                                  categoriesList[i],
+                                ),
+                                onPressed: ()=>{
+                                  print("HI"),
+                                },
+                              ).paddingOnly(right: 10, bottom: 5),
+                            ),
+                          ).paddingSymmetric(vertical: 10),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.filter_list),
+                          Text(
+                            'Sort by Date',
                             style: Theme.of(context)
                                     .textTheme
                                     .bodyText1!
                                     .copyWith(
-                                      color: Colors.blue[800],
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.black,
                                     ),
                           ),
-                          onPressed: ()=>{
-                           DateTimePicker.dateRangePicker(context)
-                          },
-                          ),
-                        )
-                      ],
-                    ),
+                          SizedBox(
+                            height: 25,
+                            child: MaterialButton(
+                              child: Text(
+                              'Select Date',
+                              style: Theme.of(context)
+                                      .textTheme
+                                      .bodyText1!
+                                      .copyWith(
+                                        color: Colors.blue[800],
+                                      ),
+                            ),
+                            onPressed: ()=>{
+                             DateTimePicker.dateRangePicker(context)
+                            },
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ).paddingSymmetric(vertical: 10),
+                ).paddingSymmetric(vertical: 10, horizontal: 20),
                   ],
-                ).paddingSymmetric(vertical: 10),
-              ).paddingSymmetric(vertical: 10, horizontal: 20),
-                ],
+                ),
               ),
-            ),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: controller.events.length,
-              itemBuilder: (_, index) {
-                return EventCard(controller.events[index]);
-              },
-            ).paddingOnly(top: 10, bottom: 15),
-            
-          ],
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: controller.events.length,
+                itemBuilder: (_, index) {
+                  return EventCard(controller.events[index]);
+                },
+              ).paddingOnly(top: 10, bottom: 15),
+              
+            ],
+          ),
         ),
       ),
     );
