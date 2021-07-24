2658a159012f210670ccadb4d90734751b021aec
Dipesh Shrestha <51238569+deep-essence347@users.noreply.github.com>
2021-07-24 21:04:52 +0545

    token send

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -42,7 +42,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.ADMIN_DASHBOARD;
+  static const INITIAL = Routes.SIGNUP;
 
   static final routes = [
     GetPage(
