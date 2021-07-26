c7435c843ed6623e0d4d05a9278798c7d6626008
sagar Uprety <sagarupreti100@gmail.com>
2021-07-26 22:41:05 +0545

    participant_list

--- lib/app/modules/event_detail/provider/event_detail_provider.dart
+++ lib/app/modules/event_detail/provider/event_detail_provider.dart
@@ -2,4 +2,5 @@ import 'package:eventrack_app/app/models/response.dart';
 
 abstract class EventDetailProvider {
   Future<ResponseModel?> registerToEvent(String id);
+  Future<ResponseModel?> getParticipants(String id);
 }
