0a5429ba9765fb570d94b6b27571985804b7e443
Shreya Shrestha <shreya2057@gmail.com>
2021-07-27 10:34:04 +0545

    My Favourite Events

--- lib/app/modules/userdashboard/bindings/userdashboard_binding.dart
+++ lib/app/modules/userdashboard/bindings/userdashboard_binding.dart
@@ -1,3 +1,6 @@
+import 'package:eventrack_app/app/controllers/controllers/global_controller.dart';
+import 'package:eventrack_app/app/modules/initLoad/controllers/init_load_controller.dart';
+import 'package:eventrack_app/app/modules/userdashboard/provider/user_dashboard_impl.dart';
 import 'package:get/get.dart';
 
 import '../controllers/userdashboard_controller.dart';
@@ -5,6 +8,8 @@ import '../controllers/userdashboard_controller.dart';
 class UserdashboardBinding extends Bindings {
   @override
   void dependencies() {
+     Get.put(GlobalController());
+     Get.put<UserProviderImpl>(UserProviderImpl());
     Get.lazyPut<UserdashboardController>(
       () => UserdashboardController(),
     );
--- lib/app/modules/userdashboard/controllers/userdashboard_controller.dart
+++ lib/app/modules/userdashboard/controllers/userdashboard_controller.dart
@@ -1,19 +1,44 @@
+import 'package:eventrack_app/app/models/response.dart';
+import 'package:eventrack_app/app/models/user/user.dart';
 import 'package:eventrack_app/app/modules/initLoad/controllers/init_load_controller.dart';
+import 'package:eventrack_app/app/modules/userdashboard/provider/user_dashboard_impl.dart';
+import 'package:eventrack_app/app/modules/userdashboard/provider/user_provider.dart';
 import 'package:get/get.dart';
 
 import '../../../models/event/event.dart';
 
 class UserdashboardController extends GetxController {
   late List<Event> events;
+  late UserDashboardProvider dashboardProvider;
+  late RxList<Event> myFavourite;
   late InitLoadController global;
 
   @override
   void onInit() async {
     global = Get.find<InitLoadController>();
-     events = global.events;
+    dashboardProvider = Get.find<UserProviderImpl>();
+    myFavourite = <Event>[].obs;
+    events = global.events;
     print(
         'User: ${global.currentUser.toJson()}\n\n\n Organizaiton: ${global.organization.toJson()}\n\n\n First Event: ${global.events.length}');
     super.onInit();
    
+  }
+
+
+  getMyFavourite()async{
+    try{
+      print('object');
+      ResponseModel? events = await dashboardProvider.getMyFavouriteEvents();
+      if(events!.state){
+        print('MyFavourite Events found');
+        myFavourite.value = events.eventList!;
+      }
+      else{
+        print('My Favourite Events not found');
+      }
+    }catch (e) {
+      print(e);
+    }
   }
 }
