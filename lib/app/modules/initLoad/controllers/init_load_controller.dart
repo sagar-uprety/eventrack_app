4b9d9c4e67367fe2affbc53711e2b231f54c1e20
Dipesh Shrestha <51238569+deep-essence347@users.noreply.github.com>
2021-07-27 13:23:49 +0545

    password reset and verification

--- lib/app/modules/initLoad/controllers/init_load_controller.dart
+++ lib/app/modules/initLoad/controllers/init_load_controller.dart
@@ -47,22 +47,22 @@ class InitLoadController extends GetxController {
     FlashMessage(response!.state,
         message: response.message, displayOnSuccess: false);
     if (response.state) {
-      await getuser(
-        user: response.user!,
-        events: response.eventList!,
-        organization: response.organization,
-      );
-      Get.toNamed(Routes.USERDASHBOARD);
-      // if (response.user!.isVerified!) {
-      //   await getuser(
-      //     user: response.user!,
-      //     events: response.eventList!,
-      //     organization: response.organization,
-      //   );
-      //   Get.offAllNamed(Routes.USERDASHBOARD);
-      // } else
-      //   Get.offAllNamed(Routes.TOKEN_VERIFCATION,
-      //       arguments: {'email': response.user!.email, 'type': 'email'});
+      // await getuser(
+      //   user: response.user!,
+      //   events: response.eventList!,
+      //   organization: response.organization,
+      // );
+      // Get.toNamed(Routes.USERDASHBOARD);
+      if (response.user!.isVerified!) {
+        await getuser(
+          user: response.user!,
+          events: response.eventList!,
+          organization: response.organization,
+        );
+        Get.offAllNamed(Routes.USERDASHBOARD);
+      } else
+        Get.offAllNamed(Routes.TOKEN_VERIFCATION,
+            arguments: {'email': response.user!.email, 'type': 'email'});
     } else
       await SharedPreference.requestLogout();
   }
--- lib/app/modules/login/provider/login_provider.dart
+++ lib/app/modules/login/provider/login_provider.dart
@@ -1,5 +1,6 @@
 import '../../../models/response.dart';
 
 abstract class LoginProvider {
-  Future<ResponseModel?> loginUser({Map<String, dynamic> data});
+  Future<ResponseModel> loginUser(Map<String, dynamic> data);
+  Future<ResponseModel> checkEmail(Map<String, dynamic> data);
 }
